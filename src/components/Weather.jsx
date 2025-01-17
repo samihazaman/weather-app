cc99f640c5a0064768c9ffab9977d522e068cde1
samihazaman <samihaz2009@gmail.com>
2025-01-17 16:35:26 -0500

    Update weather.jsx

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -39,6 +39,7 @@ const Weather = () => {
         return;
       }
 
+      //
       try {
           const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.OPEN_WEATHER_API_KEY}`;
 
